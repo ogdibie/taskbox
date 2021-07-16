3b3e8c0bd943fbea7e7f24a0dabd301d6f558dc1
Ruona Dibie <ogheneruona.dibie@gmail.com>
2021-07-16 07:49:09 -0700

    Added simply test using stories created

---
+++ src/components/TaskList.test.js
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react";
+import { composeStories } from "@storybook/testing-react";
+
+import * as TaskListStories from "./TaskList.stories"; //👈  Our stories imported here
+
+const { WithPinnedTasks } = composeStories(TaskListStories);
+
+it("renders pinned task at the start of the list", () => {
+  const { container } = render(<WithPinnedTasks />);
+  expect(
+    container.querySelector(
+      '.list-item:nth-child(1) input[value="Task 6 (pinned)"]'
+    )
+  ).not.toBe(null);
+});
