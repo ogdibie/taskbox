c2ebd9ba9312a12c6a500cda7214a4a4e0159b22
Ruona Dibie <ogheneruona.dibie@gmail.com>
2021-07-16 08:37:25 -0700

    Made Tasked list a container not presentational

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Task from "./Task";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { archiveTask, pinTask } from "../lib/redux";
 
-export default function TaskList({ loading, tasks, onPintask, onArchiveTask }) {
+export function PureTaskList({ loading, tasks, onPintask, onArchiveTask }) {
   const events = {
     onPintask,
     onArchiveTask,
@@ -53,13 +55,25 @@ export default function TaskList({ loading, tasks, onPintask, onArchiveTask }) {
   );
 }
 
-TaskList.propTypes = {
+PureTaskList.propTypes = {
   loading: PropTypes.bool,
   tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
   onPintask: PropTypes.func,
   onArchiveTask: PropTypes.func,
 };
 
-TaskList.defaultProps = {
+PureTaskList.defaultProps = {
   loading: false,
 };
+
+export default connect(
+  ({ tasks }) => ({
+    tasks: tasks.filter(
+      (t) => t.state === "TASK_INBOX" || t.state === "TASK_PINNED"
+    ),
+  }),
+  (dispatch) => ({
+    onArchiveTask: (id) => dispatch(archiveTask(id)),
+    onPinTask: (id) => dispatch(pinTask(id)),
+  })
+)(PureTaskList);
